5d798cf0eadf0e664cfd5da5274ba211653c6d3a
amal <amalpaul0492@gmail.com>
2016-10-30 19:01:58 +0530

    initial commit

---
+++ app/src/main/java/com/amal/imagepicker/MainActivity.java
@@ -0,0 +1,139 @@
+package com.amal.imagepicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int PICK_IMAGE = 12345;
+    private final int TAKE_PICTURE = 6352;
+    private Bitmap bitmap;
+
+    private ImageView imageView;
+    private Button fromCamera, fromGallery, upload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        fromCamera = (Button) findViewById(R.id.fromCamera);
+        fromGallery = (Button) findViewById(R.id.fromGallery);
+        upload = (Button) findViewById(R.id.upload);
+        upload.setOnClickListener(this);
+        fromCamera.setOnClickListener(this);
+        fromGallery.setOnClickListener(this);
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            fromCamera.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fromCamera:
+                getImageFromCamera();
+                break;
+            case R.id.fromGallery:
+                getImageFromGallery();
+                break;
+            case R.id.upload:
+                if (bitmap != null)
+                    uploadImageToServer();
+                break;
+        }
+    }
+
+    private void uploadImageToServer() {
+        File imageFile = persistImage(bitmap, "profile");
+        Ion.with(this)
+                .load("https://koush.clockworkmod.com/test/echo")  // replace it with your upload url
+                .setMultipartFile("profile_pic", "image/jpeg", imageFile)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+
+                    }
+                });
+    }
+
+    private File persistImage(Bitmap bitmap, String name) {
+        File filesDir = getApplicationContext().getFilesDir();
+        File imageFile = new File(filesDir, name + ".jpg");
+
+        OutputStream os;
+        try {
+            os = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+        }
+
+        return imageFile;
+    }
+
+
+    private void getImageFromCamera() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePicture, TAKE_PICTURE);
+        }
+    }
+
+    private void getImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } else if (requestCode == TAKE_PICTURE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Bundle extras = data.getExtras();
+                bitmap = (Bitmap) extras.get("data");
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+}
